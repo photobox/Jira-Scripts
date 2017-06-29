ac7479d2e15e20d181a2dbd26e4b619d4aa3c7bf
Dinis Cruz <dinis.cruz@owasp.org>
2017-06-29 09:27:03 +0100

    Update risk-decision.js

--- src/confluence/risk-decision.js
+++ src/confluence/risk-decision.js
@@ -1,9 +1,8 @@
 jira_Url = 'http://jira.photobox.com/rest/api/latest/issue/' + jira_Id ;
 
-$('#Issue_Link').html('<a href=http://jira.photobox.com/browse/' + jira_Id + '>' + jira_Id + '</a>')
-
 $.getJSON(jira_Url, function(data) 
 { 
+	$('#Issue_Link'              ).html('<a href=http://jira.photobox.com/browse/' + jira_Id + '>' + jira_Id + '</a>')
 	
 	$('#Summary'                 ).html(data.fields.summary          ); 
 	$('#Description'      	     ).html(data.fields.description      ); 
